7a13a52402a9bcd4a0377c13dd588e36bd665b5d
Craig <craig@oppkey.com>
2021-10-18 11:44:38 -0700

    put download command in loop

--- sc2_download.sh
+++ sc2_download.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
+
 URL=http://192.168.1.1/files/thetasc26c21a247d9055838792badc5/100RICOH/R0012311.MP4
 
-curl $URL --output R0012311.MP4
-
+for i in {1..5}
+do
+    echo "Start test $i"
+    curl $URL --output R0012311.MP4
+    echo "completed test $i"
+    echo "-------------"
+done
